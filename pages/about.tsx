d5869d180f6e5af931362a2eef4a2be532c16b3f
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-07-13 19:21:34 -0400

    about page init

---
+++ pages/about.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import styles from '../styles/About.module.css';
+import Navbar from '../components/Navbar';
+import { PATHS } from '../utilities/Constants';
+
+export default function About() {
+    return (
+        <div className={styles.root}>
+            <Head>
+                <title>Ben Vandenberg: About</title>
+                <meta name='description' content='A resume site' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+
+            <main>
+                <div id='home' className={styles.home}>
+                    <Navbar links={PATHS} currentPage={'About'} />
+
+                    <h1 className={`${styles.text} + ${styles.title}`}>
+                        {'About Me'}
+                    </h1>
+
+                    <p className={`${styles.text} + ${styles.description}`}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quod modi sed expedita, nam cumque corrupti vero quis!
+                        Ea officia earum beatae eaque fuga eligendi provident
+                        ullam rerum, ad recusandae quam?
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+}
--- utilities/Constants.ts
+++ utilities/Constants.ts
@@ -2,7 +2,7 @@ import { Path } from '../components/Navbar';
 
 export const PATHS: Path[] = [
     { label: 'Home', href: '/' },
-    { label: 'About', href: '/' },
-    { label: 'Resume', href: '/' },
+    { label: 'About', href: '/about' },
+    { label: 'Resume', href: '/resume' },
     { label: 'Skills', href: '/skills' },
 ];
