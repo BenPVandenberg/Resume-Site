ab1c8975e7430bd5579a6a0261938bbdc50d8af7
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-07-19 19:29:36 -0400

    init animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar';
@@ -14,20 +15,45 @@ export default function Home() {
 
             <main>
                 <div id='home' className={styles.home}>
-                    <Navbar links={PATHS} currentPage={'Home'} />
-
-                    <div id='topText' className={styles.text}>
-                        <h1 className={styles.title}>
+                    <Navbar links={PATHS} currentPage={'Resume'} />
+                    <AnimatePresence>
+                        <motion.div
+                            id='topText'
+                            className={styles.text}
+                            initial='hidden'
+                            animate='visible'
+                            exit='hidden'
+                            variants={{
+                                hidden: {
+                                    scale: 0.8,
+                                    opacity: 0,
+                                    transition: {
+                                        delay: 0.4,
+                                        duration: 2,
+                                    },
+                                },
+                                visible: {
+                                    scale: 1,
+                                    opacity: 1,
+                                    transition: {
+                                        delay: 0.4,
+                                    },
+                                },
+                            }}
+                        >
+                            <motion.h1 className={styles.title}>
                             {"I'm Ben Vandenberg."}
-                        </h1>
+                            </motion.h1>
 
-                        <p className={styles.description}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quod modi sed expedita, nam cumque corrupti
-                            vero quis! Ea officia earum beatae eaque fuga
-                            eligendi provident ullam rerum, ad recusandae quam?
-                        </p>
-                    </div>
+                            <motion.p className={styles.description}>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Quod modi sed expedita, nam
+                                cumque corrupti vero quis! Ea officia earum
+                                beatae eaque fuga eligendi provident ullam
+                                rerum, ad recusandae quam?
+                            </motion.p>
+                        </motion.div>
+                    </AnimatePresence>
                 </div>
             </main>
         </div>
