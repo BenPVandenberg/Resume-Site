1c42cf5a550f1f71b1893d0f4806c0a05a194940
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-07-20 00:58:45 -0400

    forgot favicon references

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,34 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Head>
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='public/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='public/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='public/favicon-16x16.png'
+                />
+                <link rel='icon' href='/favicon.ico' />
+                <link rel='manifest' href='/manifest.json' />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 export default MyApp;
