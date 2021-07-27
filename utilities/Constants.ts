1040ef6c7070ff657d64bfd705dc9d1c4582d4a3
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-07-26 21:14:46 -0400

    Delete Constants.ts

--- utilities/Constants.ts
+++
@@ -1,8 +0,0 @@
-import { Path } from '../components/Navbar';
-
-export const PATHS: Path[] = [
-    { label: 'Home', href: '/' },
-    { label: 'About', href: '/about' },
-    { label: 'Resume', href: '/resume' },
-    { label: 'Skills', href: '/skills' },
-];
