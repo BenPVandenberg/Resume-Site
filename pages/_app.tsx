7bce39320249e3dbd8ed48b717b2f2110ea433c5
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2022-01-13 14:26:08 -0500

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,6 +31,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                     property='og:image'
                     content='/android-chrome-192x192.png'
                 />
+
+                {/* Global Site Tag (gtag.js) - Google Analytics */}
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                        page_path: window.location.pathname,
+                        });
+                        `,
+                    }}
+                />
             </Head>
             <Component {...pageProps} />
         </div>
